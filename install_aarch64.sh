6e8608c4d7acffc92854aa2541a911f68c8a0a12
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-10-20 16:56:10 +0100

    Create install_aarch64.sh

---
+++ install_aarch64.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+echo "Starting Install Of 'sandcount'..."
+echo "Downloading Stuff..."
+mkdir -p /usr/share/sandpotnoodles
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/hRR-xr4W.png -O /usr/share/sandpotnoodles/sandcount.png
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/sandcount.desktop -O /usr/share/applications/sandcount.desktop
+wget https://github.com/ActuallySandPotNoodles/sandcount/releases/download/release/sandcount_aarch64 -O /usr/bin/sandcount
+chmod 775 /usr/bin/sandcount
+echo "Install Complete!"

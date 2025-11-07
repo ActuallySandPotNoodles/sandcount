ac04209295120c2b7a3c13616d12a92005ebdb04
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-11-07 09:29:32 +0000

    Fix download link for sandcount icon in install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 echo "Downloading Stuff..."
 mkdir -p /usr/share/sandpotnoodles
-wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/hRR-xr4W.png -O /usr/share/sandpotnoodles/sandcount.png
+wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/appicon.png -O /usr/share/sandpotnoodles/sandcount.png
 wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/sandcount.desktop -O /usr/share/applications/sandcount.desktop
 wget https://github.com/ActuallySandPotNoodles/sandcount/releases/download/release-1.1/sandcount_x86_64 -O /usr/bin/sandcount
 chmod 775 /usr/bin/sandcount

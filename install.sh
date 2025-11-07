e3671548e785fe89fa2cbd9daf5ffccad1b9f816
SandPotNoodles <77805958+ActuallySandPotNoodles@users.noreply.github.com>
2025-11-07 09:20:51 +0000

    I updated download link for sandcount binary

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ echo "Downloading Stuff..."
 mkdir -p /usr/share/sandpotnoodles
 wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/hRR-xr4W.png -O /usr/share/sandpotnoodles/sandcount.png
 wget https://raw.githubusercontent.com/ActuallySandPotNoodles/sandcount/refs/heads/main/sandcount.desktop -O /usr/share/applications/sandcount.desktop
-wget https://github.com/ActuallySandPotNoodles/sandcount/releases/download/release/sandcount_x86_64 -O /usr/bin/sandcount
+wget https://github.com/ActuallySandPotNoodles/sandcount/releases/download/release-1.1/sandcount_x86_64 -O /usr/bin/sandcount
 chmod 775 /usr/bin/sandcount
 echo "Install Complete!"
